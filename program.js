287cfd8bbe17e6c01559f5199cf6b32d1d7f4abb
Adam O <adamorr234@gmail.com>
2021-06-22 11:33:38 -0600

    pruned files and added secure .env var config

--- program.js
+++ program.js
@@ -1,75 +1,101 @@
 require('dotenv').config();
-const express = require('express');
-const {MongoClient} = require('mongodb');
-const server = express();
+var express = require('express');
+var { MongoClient, ObjectID } = require('mongodb');
+var server = express();
 
-var dbUrl = process.env.DATABASE_URL;
+var dbUri =
+  process.env.DATABASE_URL;
+
 var reg1 = /\w{4,5}:\/\/\w+.\w+/;
 
 server.use(express.static('view'));
 
 function generateShortUrl() {
-  return Math.floor((Math.random() * 5000) + 1);
+  return Math.floor(Math.random() * 5000 + 1);
 }
 
-server.get('/:shortUrl', function(req, res) {
-  var url = req.params.shortUrl;
-  var fullShortUrl = 'https://miniurl.herokuapp.com/' + url;
-  mongoClient.connect(dbUrl, function(err, db) {
-    if(err) throw err;
-    
-    db.collection('urldb').find({"short_url": fullShortUrl}).toArray(function(error, docs) {
-      if(error) throw error;
-      if(docs[0]) {
-        res.redirect(docs[0].original_url);
+async function addDocument(baseURL, url, client) {
+  try {
+    const docRes = await client
+      .db()
+      .collection('urldb')
+      .insert({
+        original_url: url,
+        short_url: baseURL + generateShortUrl(),
+      });
+    return docRes;
+  } catch (e) {
+    console.error(e);
+  }
+}
+async function findDocument(query, client) {
+
+  const options = { original_url: 1, short_url: 1, _id: 0 };
+  var doc;
+  try {
+    doc = await client.db().collection('urldb').findOne(query, options);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    console.log('line 45 at findDocument finally', doc);
+    return doc;
+  }
+}
+
+server.get('/new/*', function (req, res) {
+  const originalUrl = req.params[0];
+  //refactor note: add baseurl to req.params at base request for resuability
+  const baseURL = 'http://' + req.headers.host + '/';
+
+  if (reg1.test(originalUrl)) {
+    MongoClient.connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+      .then((client) => {
+        findDocument({ original_url: originalUrl }, client).then((doc) => {
+          if (doc) {
+            res.send(doc);
+          } else {
+            //maybe refactor to pass  monogdb formatted document rather than just values
+            addDocument(baseURL, originalUrl, client).then((docRes) => {
+              res.send(docRes.ops[0]);
+            });
+          }
+        });
+      })
+      .catch((err) => console.error(err));
+  } else {
+    res.send('{"error": "Invalid url"}');
+  }
+});
+
+server.get('/:shortUrl', function (req, res) {
+  const shortUrl = req.params.shortUrl;
+  const baseUrl = 'http://' + req.headers.host + '/';
+  const fullShortUrl = baseUrl + shortUrl;
+
+  //Longest unslove bug: 4 hours. I didn't know dbUri was pointing to wrong database /short_urls rather than the correct /urldb that's why All my find queries returned null
+  //Oh what time I have wasted for lack of care. looking at each and every variable led me the hard and long way.
+
+  MongoClient.connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then((client) => {
+    findDocument({ short_url: fullShortUrl }, client).then((doc) => {
+      if (doc) {
+        res.redirect(doc.original_url);
       } else {
-        res.end();
+        res.status(404);
+        res.end('resource not found');
       }
     });
-    db.close();
   });
 });
 
-server.get('/new/*', function(req, res) {
-  var originalUrl = req.params[0];
-  
-  function add_document(url) {
-    mongoClient.connect(dbUrl, function(err, db) {
-    if(err) throw err;
-  
-    db.collection('urldb').insertOne({
-      original_url: url, 
-      short_url: 'https://miniurl.herokuapp.com/' + generateShortUrl()
-    }, function(err, response) {
-      if(err) throw err;
-      res.send(response.ops[0]);
-    });
-    db.close();
-  });
-  }
-
-  if(reg1.test(originalUrl)) {
-    mongoClient.connect(dbUrl, function(err, db) {
-      if(err) throw err;
-      db.collection('urldb').find({"original_url": originalUrl}, {"original_url": 1, "short_url": 1, "_id": 0}).toArray(function(err, docs) {
-        if(err) throw err;
-        if(docs.length === 0) {
-          add_document(originalUrl); 
-        } else {
-          res.send(JSON.stringify(docs[0]));
-        }
-      });
-      
-      db.close();
-    });
- } else {
-   res.send('{"error": "Invalid url"}');
- }
-  
+server.listen(process.env.PORT || 3000, process.env.IP, function (err, res) {
+  if (err) console.log('Error in server setup');
+  console.log('Server listening on Port', process.env.PORT);
 });
 
-server.listen((process.env.PORT || 3000), process.env.IP, function(err, res){
-  if (err) console.log("Error in server setup")
-  console.log("Server listening on Port", process.env.PORT);
-});
-console.log('programm.js file ran successfully')
+console.log('dev.js file ran successfully');
